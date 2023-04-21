28fe7f95bd477608dc790882414ce075770f29c6
enhoker <45732134+enhoker@users.noreply.github.com>
2023-04-21 23:56:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,10 +10,7 @@ def solution(x_success: int,
              y_success: int, 
              y_cnt: int) -> bool:
     list_for_test = [[x_cnt-x_success, x_success],[y_cnt - y_success, y_success]]
-    try:
-                stat, p, dof, expected = chi2_contingency(list_for_test)
-            except:
-                p = -1
+    stat, p, dof, expected = chi2_contingency(list_for_test)
     a = True #оставляем канал
     if (y_success / y_cnt > x_success / x_cnt):
       a = False #убираем канал
